f7eb29c7e90c6bbbf84ba2aafce07187b524ce31
NIRMAL-BISHT <67547558+NIRMAL-BISHT@users.noreply.github.com>
2025-06-09 17:33:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ spark = SparkSession.builder.appName("NIELSEN_CASE_STUDY").getOrCreate()
 res = cbsodata.get_data('84910NED',filters = "(RegioS eq 'PV20  ' or RegioS eq 'PV21  ' or RegioS eq 'PV22  ' or RegioS eq 'PV23  ' or RegioS eq 'PV24  ' or RegioS eq 'PV25  ' or RegioS eq 'PV26  ' or RegioS eq 'PV27  ' or RegioS eq 'PV28  ' or RegioS eq 'PV29  ' or RegioS eq 'PV30  ' or RegioS eq 'PV31  ' or RegioS eq 'NL01  ' or RegioS eq 'GM0513' or RegioS eq 'GM0518') and (Perioden eq '2021JJ00')")
 df = spark.createDataFrame(res)
 
+# Task1 - Fetching all the residents from 12 dutch provinces who are from 1st genration migration and are from China
 china_df = df.where((df.Migratieachtergrond =='China') & (df.Generatie == '1e generatie migratieachtergrond') &  (df.Leeftijd == 'Totaal')  &  (df.Geslacht == 'Totaal mannen en vrouwen')).select("RegioS","BevolkingOp1Januari_1")
 
 df1 = china_df.where('RegioS not in ("\'s-Gravenhage (gemeente)","Gouda","Nederland")')
-
 df1.show()
 
+# Task2 - Creating dataframe from china_df and calculating average of 12 dutch provinces and then average of nederaland data.
 df2 = (china_df 
     .agg(
         avg(when(col("RegioS").endswith("(PV)"), col("BevolkingOp1Januari_1"))).alias("weighted_province_average"),
@@ -20,8 +21,34 @@ df2 = (china_df
     ) )
 df2.show()
 
-
-india_df = df.where((df.Migratieachtergrond =='India') & (df.Generatie == '1e generatie migratieachtergrond') &  (df.Leeftijd.isin('20 tot 25 jaar','25 tot 30 jaar','30 tot 35 jaar','35 tot 40 jaar','40 tot 45 jaar','45 tot 50 jaar'))  &  (df.Geslacht == 'Vrouwen')).groupBy("RegioS").sum("BevolkingOp1Januari_1")
+# Task-3 Creating a dataframe to fetch record of 12 dutch provinces plus Gouda municaiplaity which were 1st generation migration and were from India and aged between 20 to 50
+india_df = df.where((df.Migratieachtergrond =='India') & (df.Generatie == '1e generatie migratieachtergrond') &  (df.Leeftijd.isin('20 tot 25 jaar','25 tot 30 jaar','30 tot 35 jaar','35 tot 40 jaar','40 tot 45 jaar','45 tot 50 jaar'))  &  (df.Geslacht == 'Vrouwen')).groupBy("RegioS").agg(sum("BevolkingOp1Januari_1").alias("BevolkingOp1Januari_1"))
 df3 = india_df.where(~india_df.RegioS.isin("\'s-Gravenhage (gemeente)","Nederland"))
 
+
 df3.show()
+
+
+df4 =  df.where((df.Migratieachtergrond =='Totaal') & (df.Generatie == 'Totaal') &  (df.Leeftijd == 'Totaal')  &  (df.Geslacht == 'Totaal mannen en vrouwen')).select("RegioS","BevolkingOp1Januari_1")
+df5 = df4.alias("tbl_1").join(india_df.alias("tbl_2") , df4.RegioS == india_df.RegioS, "inner").select("tbl_1.RegioS",expr("tbl_2.BevolkingOp1Januari_1/tbl_1.BevolkingOp1Januari_1 as share"),expr("tbl_2.BevolkingOp1Januari_1 as indian_reisdents"),expr("tbl_1.BevolkingOp1Januari_1 as total_resident"))
+
+
+gouda_share = df5.filter(col("RegioS") == "Gouda").select("share").first()["share"]
+
+# Filter only provinces (exclude Gouda and Nederland and municipalities)
+provinces_df = df5.filter(
+    (~col("RegioS").isin("Gouda", "Nederland", "'s-Gravenhage (gemeente)"))
+)
+# Add column with absolute difference from Gouda's share
+provinces_df = provinces_df.withColumn("diff", abs(col("share") - lit(gouda_share)))
+# Get the province with the minimum difference
+closest_province = provinces_df.orderBy("diff").limit(1)
+# Get Gouda's row
+gouda_df = df5.filter(col("RegioS") == "Gouda")
+# Combine the two rows
+result_df = gouda_df.unionByName(closest_province.select(df5.columns)).select("RegioS","share")
+
+
+result_df.show(truncate=False)
+
+spark.stop()
